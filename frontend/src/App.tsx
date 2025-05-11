e71c6c40edac687a57b1db2539f24a07b54c0fc1
KritimanOjah <okritiman@gmail.com>
2025-05-11 10:59:04 +0530

    apple

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Buyer from './components/Dashboard/Buyer';
+
+const App : React.FC = () => {
+  return (
+    <div>
+      <Buyer/>
+    </div>
+  )
+}
+
+export default  App;
---
+++ frontend/src/components/Dashboard/Buyer.tsx
@@ -0,0 +1,17 @@
+//Landing page
+
+import React from 'react'
+
+const Buyer : React.FC = () => {
+  return (
+    <>
+      <div className=''>
+        
+      </div>
+    </>
+  )
+}
+
+export default Buyer;
+
+''
